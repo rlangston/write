9cf470013bd931fad40feab75ae254438449876c
Robert Langston <robertlangston@gmail.com>
2023-05-07 11:17:12 +0100

    Recalculate window bounds on resize.

--- write.c
+++ write.c
@@ -3,10 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define CTRL(x) ((x) & 0x1f)
-
-#define MAX_FILENAME_LENGTH 255
-#define MAX_COMMAND_LENGTH 255
+#include "write.h"
 
 // Window size
 int windowx, windowy;
@@ -14,107 +11,29 @@ WINDOW *textscr;
 WINDOW *statusscr;
 WINDOW *commandscr;
 
-// Modes
-#define MODE_EDIT 1
-#define MODE_COMMAND 2
+// Current mode
 int mode;
-
-// Options
-bool show_linenumbers = false;
-
-// Colours
-#define COL_WHITEBLUE 1
-#define COL_GREENBLACK 2
 
 char *filename;
 
-// Line structure (a double linked list)
-typedef struct Line {
-	size_t length;
-	struct Line *prev;
-	struct Line *next;
-	char *text;
-} Line;
+// Modes
+#define MODE_EDIT 1
+#define MODE_COMMAND 2
 
-typedef struct buffer {
-	char *filename;
-	Line *first_line;
-	Line *current_line;
-	Line *first_screen_line;
-	ssize_t lines;
-	int cx;
-	int cy;
-	int margin_left;
-	int offsetx;
-	int offsety;
-	bool modified;
-	struct buffer *next;
-} buffer;
+bool show_linenumbers = false;
+int display_cx;
 
 buffer *current_buffer = NULL;
-
 buffer *paste_buffer = NULL;
-
 buffer *message_buffer= NULL;
-int message_timer = 0;
-
 buffer *first_buffer = NULL;
+int message_timer = 0;
 
 // Cut and paste buffer
 Line *pastebuffer;
 
-// Functions
-void move_right();
-void move_left();
-void move_up();
-void move_down();
-void move_word_right();
-void move_word_left();
-void move_page_down();
-void move_page_up();
-void move_end();
-void move_home();
-void move_file_home();
-void move_file_end();
-
-void check_boundx();
-void goto_line(ssize_t line);
-void move_lines_up(int count);
-void move_lines_down(int count);
-
-void copy_line();
-void cut_line();
-void paste_line();
-void delete_line(Line *line);
-void delete_lines(Line *start_line);
-
-bool get_input(char *prompt, char *placeholder, char *response, size_t max_length);
-bool open_file(char *open_filename);
-void save_file(char *save_filename);
-void new_file(char *new_filename);
-void init();
-void shutdown();
-void close(buffer *close_buffer);
-void prompt_save();
-
-void draw_screen();
-void update_status();
-void message(char *msg);
-void draw_line(int y, Line *line);
-void toggle_linenumbers();
-
-void insert_string(Line *line, int pos, char *src, int length);
-void allocate_string(Line *line, int length);
-Line *insert_line(Line *prev, Line *next, char *src, size_t length);
-void insert_char(Line *line, int position, char c);
-void enter();
-void backspace();
-void delete();
-
 // Last pressed key
 int ch;
-
-buffer *add_buffer();
 
 int main(int argc, char *argv[])
 {
@@ -133,7 +52,7 @@ int main(int argc, char *argv[])
 	first_buffer = current_buffer;
 
 	// Create message buffer
-	message_buffer = add_buffer();
+	message_buffer = add_sbuffer();
 	message_buffer->first_line = NULL;
 	message_buffer->lines = 0;
 
@@ -146,8 +65,10 @@ int main(int argc, char *argv[])
 	
 	while(current_buffer != NULL)
 	{
-		wmove(stdscr, current_buffer->cy, current_buffer->cx - current_buffer->offsetx + current_buffer->margin_left); // Move cursor to position
 		draw_screen();
+		// wmove(stdscr, current_buffer->cy, current_buffer->cx - current_buffer->offsetx + current_buffer->margin_left); // Move cursor to position
+		wmove(stdscr, current_buffer->cy, display_cx); // Move cursor to position
+		wrefresh(textscr);
 		update_status();
 
 		ch = getch();
@@ -251,6 +172,9 @@ int main(int argc, char *argv[])
 					// execute command
 				}
 				break;
+			case KEY_RESIZE:
+				resize_window();
+				break;
 
 			// Editing
 			case 10: // ENTER
@@ -266,7 +190,7 @@ int main(int argc, char *argv[])
 				current_buffer->modified = true;
 				break;
 			default:
-				if (ch > 27 && ch < 256) // Ignore control characters
+				if ((ch > 27 && ch < 256) || (ch == '\t')) // Ignore control characters
 				{
 					insert_char(current_buffer->current_line, current_buffer->cx, ch);
 					current_buffer->cx++;
@@ -286,9 +210,7 @@ void init()
 	// Initialise screen and get dimensions
 	initscr();
 	set_escdelay(1);
-
-	getmaxyx(stdscr, windowy, windowx);
-	windowy -= 2; // Reduce for status bar and message buffer
+	resize_window();
 
 	textscr = subwin(stdscr, windowy, windowx, 0, 0);
 	statusscr = subwin(stdscr, 1, windowx, windowy, 0);
@@ -574,7 +496,7 @@ void draw_screen()
 		line = line->next;
 		y++;
 	}
-	wrefresh(textscr);
+
 	return;
 }
 
@@ -589,6 +511,7 @@ void draw_line(int y, Line *line)
 
 	wmove(textscr, y, current_buffer->margin_left);
 	int x = 0;
+	int dx = 0;
 	while ((x + current_buffer->offsetx < line->length) && x < windowx - current_buffer->margin_left)
 	{
 		if (isdigit(line->text[x + current_buffer->offsetx]))
@@ -597,11 +520,22 @@ void draw_line(int y, Line *line)
 			waddch(textscr, line->text[x + current_buffer->offsetx]);
 			wattroff(textscr, COLOR_PAIR(2));
 		}
+		else if (line->text[x + current_buffer->offsetx] == '\t')
+		{
+			for (int i = 0; i < TAB_SIZE; i++) waddch(textscr, ' ');
+			// NEED TO DEAL WITH THIS
+			if (current_buffer->cx - current_buffer->offsetx > x) dx += TAB_SIZE - 1;
+		}
 		else
 			waddch(textscr, line->text[x + current_buffer->offsetx]);
 		x++;
 	}
 	wclrtoeol(textscr);
+
+	if (line == current_buffer->current_line)
+	{
+		display_cx = current_buffer->cx - current_buffer->offsetx + current_buffer->margin_left + dx;
+	}
 
 	return;
 }
@@ -902,12 +836,7 @@ void new_file(char *new_filename)
 buffer *add_buffer()
 {
 	buffer *new_buffer;
-	new_buffer = (buffer *) malloc(sizeof(buffer));
-	new_buffer->first_line = NULL;
-	new_buffer->current_line = NULL;
-	new_buffer->first_screen_line = NULL;
-	new_buffer->lines = 0;
-	new_buffer->filename = NULL;
+	new_buffer = add_sbuffer();
 	if (current_buffer == NULL)
 		new_buffer->next = NULL;
 	else
@@ -915,7 +844,18 @@ buffer *add_buffer()
 		new_buffer->next = current_buffer->next;
 		current_buffer->next = new_buffer;
 	}
+	return new_buffer;
+}
 
+buffer *add_sbuffer()
+{
+	buffer *new_buffer;
+	new_buffer = (buffer *) malloc(sizeof(buffer));
+	new_buffer->first_line = NULL;
+	new_buffer->current_line = NULL;
+	new_buffer->first_screen_line = NULL;
+	new_buffer->lines = 0;
+	new_buffer->filename = NULL;
 	return new_buffer;
 }
 
@@ -956,3 +896,8 @@ void prompt_save()
 	}
 }
 
+void resize_window()
+{
+	getmaxyx(stdscr, windowy, windowx);
+	windowy -= 2; // Reduce for status bar and message buffer
+}
